4a746274d3011b6a933894d42c57f5e0d6e1fa28
lilijinjin <40656605+MPS-Net@users.noreply.github.com>
2022-07-23 16:26:55 +0800

    Update install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -6,6 +6,8 @@ echo "Activating virtual environment"
 
 source $PWD/mps-env/bin/activate
 
+python -m pip install --upgrade pip
+
 $PWD/mps-env/bin/pip install numpy==1.17.5 torch==1.4.0 torchvision==0.5.0
 $PWD/mps-env/bin/pip install git+https://github.com/giacaglia/pytube.git --upgrade
 $PWD/mps-env/bin/pip install -r requirements.txt
