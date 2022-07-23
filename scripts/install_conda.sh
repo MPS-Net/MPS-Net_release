bbcb97af93e7ce55dad2785206a55afd4ecbc53b
lilijinjin <40656605+MPS-Net@users.noreply.github.com>
2022-07-23 15:15:13 +0800

    Update install_conda.sh

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -8,14 +8,9 @@ conda create -n $CONDA_ENV_NAME python=3.7
 eval "$(conda shell.bash hook)"
 conda activate $CONDA_ENV_NAME
 
-python -m pip install --upgrade pip setuptools wheel
-
 which python
 which pip
 
-conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
-
-pip install numpy==1.17.5
+pip install numpy==1.17.5 torch==1.4.0 torchvision==0.5.0
 pip install git+https://github.com/giacaglia/pytube.git --upgrade
 pip install -r requirements.txt
-pip install gdown
