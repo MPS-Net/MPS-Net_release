6dad90cbe568c02fbb50f2f6d139fa133f98d9ca
lilijinjin <40656605+MPS-Net@users.noreply.github.com>
2022-07-23 15:46:16 +0800

    Delete install_conda.sh

--- scripts/install_conda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-conda create -n mps-env python=3.7
-
-conda activate mps-env
-
-pip install numpy==1.17.5 torch==1.4.0 -f https://download.pytorch.org/whl/torch_stable.html torchvision==0.5.0
-pip install git+https://github.com/giacaglia/pytube.git --upgrade
-pip install -r requirements.txt
