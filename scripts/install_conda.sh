5f8292f7ab7c84a114d129eade5c8ab34d2c9b29
lilijinjin <40656605+MPS-Net@users.noreply.github.com>
2022-07-27 18:17:40 +0800

    Add files via upload

---
+++ scripts/install_conda.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export CONDA_ENV_NAME=mps-env
+echo $CONDA_ENV_NAME
+
+conda create -n $CONDA_ENV_NAME python=3.7
+
+eval "$(conda shell.bash hook)"
+conda activate $CONDA_ENV_NAME
+
+which python
+which pip
+
+pip install numpy==1.17.5 torch==1.4.0 -f https://download.pytorch.org/whl/torch_stable.html torchvision==0.5.0
+pip install git+https://github.com/giacaglia/pytube.git --upgrade
+pip install -r requirements.txt
